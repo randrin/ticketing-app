3758591e7a5b1c78d78dbe721bdae59fc3da73a3
Randrin Nzeukang <nrandrin@eservicios.indracompany.com>
2022-09-28 20:03:14 +0200

    Database modeling model

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import "express-async-errors";
+import mongoose from "mongoose";
 import { json } from "body-parser";
 import { currentUserRouter } from "./routers/current-user";
 import { signupRouter } from "./routers/signup";
@@ -17,12 +18,23 @@ app.use(signupRouter);
 app.use(signinRouter);
 app.use(signoutRouter);
 
-app.all('*', async (req, res) => {
+app.all("*", async (req, res) => {
   throw new NotFoundError();
 });
 
 app.use(errorHandler);
 
-app.listen(3000, () => {
-  console.log("Listing to port 3000");
-});
+const start = async () => {
+  try {
+    await mongoose.connect("mongodb://auth-mongo-srv:27017/auth");
+    console.log("Connected to MongoDb");
+  } catch (err) {
+    console.error(err);
+  }
+
+  app.listen(3000, () => {
+    console.log("Listening on port 3000!!!!!!!!");
+  });
+};
+
+start();
